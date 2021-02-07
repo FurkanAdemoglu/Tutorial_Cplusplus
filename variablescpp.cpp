d6b524a28c592b9a6d1306bf77ffa95cd80f4a28
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-02-07 21:44:13 +0300

    Variales in the C++ programming language

---
+++ variablescpp.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+
+int main() {
+ 	int myNum = 5;               // Integer (whole number without decimals)
+double myFloatNum = 5.99;    // Floating point number (with decimals)
+char myLetter = 'D';         // Character
+string myText = "Hello";     // String (text)
+bool myBoolean = true;       // Boolean (true or false)	
+
+
+//Declare Multiple Variables
+int x = 5, y = 6, z = 50;
+cout << x + y + z;
+
+//We set the const we can not change the values
+const int minutesPerHour = 60;
+const float PI = 3.14;
+
+
+
+
+  return 0;
+}
